4038fb6917eb22780cd180b13667facc3ea01a0b
Mei Xuesong <meixuesong@gmail.com>
2019-09-18 10:15:06 +0800

    init springboot project

---
+++ src/main/java/cn/meixs/aggregatepersistence/AggregatePersistenceApplication.java
@@ -0,0 +1,13 @@
+package cn.meixs.aggregatepersistence;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AggregatePersistenceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AggregatePersistenceApplication.class, args);
+    }
+
+}
